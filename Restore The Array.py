d2ef1d01548f3c2d51106f9f1ea62816e500af6a
Thanvi-kasi <kasithanvi01@gmail.com>
2026-07-16 23:17:07 +0530

    Create Restore The Array.py

---
+++ Restore The Array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfArrays(self, s: str, k: int) -> int:
+        MOD = 10**9 + 7
+        n = len(s)
+
+        dp = [0] * (n + 1)
+        dp[n] = 1
+
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                continue
+
+            num = 0
+            for j in range(i, min(n, i + 10)):
+                num = num * 10 + int(s[j])
+
+                if num > k:
+                    break
+
+                dp[i] = (dp[i] + dp[j + 1]) % MOD
+
+        return dp[0]
