0eb310dd37c15e78407fc1136067319c486acad5
Thanvi-kasi <kasithanvi01@gmail.com>
2026-07-19 19:25:21 +0530

    Create Degree of an Array.py

---
+++ Degree of an Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        freq = {}
+        first = {}
+        last = {}
+
+        for i, num in enumerate(nums):
+            if num not in first:
+                first[num] = i
+            last[num] = i
+            freq[num] = freq.get(num, 0) + 1
+
+        degree = max(freq.values())
+        ans = len(nums)
+
+        for num in freq:
+            if freq[num] == degree:
+                ans = min(ans, last[num] - first[num] + 1)
+
+        return ans
